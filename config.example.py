541f802f9ba30ae06612381ee5764f1ad01fc348
Paxton <parth8454prajapati@gmail.com>
2026-07-23 20:30:10 +0530

    Update config.example.py

--- config.example.py
+++ config.example.py
@@ -3,6 +3,9 @@ from groq import AsyncGroq
 
 os.environ["GROQ_API_KEY"] = "your-api-key-here"
 
+# guys tool calling is not reliable on "llama-3.3-70b-versatile" it often fails and its a problem 
+# from model's end hence dont waste time fixing this, thankyou!
+
 MODELS = ["openai/gpt-oss-120b","llama-3.1-8b-instant","llama-3.3-70b-versatile","openai/gpt-oss-20b"]
 
 TEXT_MODEL = MODELS[3]
